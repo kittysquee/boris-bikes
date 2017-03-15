831716455409198cc6fa89e1f396460a8b32e3f9
kittysquee <kittysquee@gmail.com>
2017-03-15 16:19:34 +0000

    Adds raising exeptions

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,32 @@ require "docking_station"
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-    it "releases a bike when release_bike is called" do
-      bike = subject.release_bike
-      expect(bike).to be_working
+
+    describe '#release_bike' do
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+      it 'raises an error if there are no bikes' do
+        expect {subject.release_bike}.to raise_error("No bikes available")
+      end
     end
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it { is_expected.to respond_to(:bike)}
+
+    it 'docks a bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
+
 end
