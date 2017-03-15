e9daa27fe0d862ad5432b7ac3291bf2492019cd8
kittysquee <kittysquee@gmail.com>
2017-03-15 13:14:01 +0000

    Makes docking station get bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require "docking_station"
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
+    it "releases a bike when release_bike is called" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
 end
