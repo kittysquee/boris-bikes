2ae90208a80bf3fc8031b0ba28aab0ba8165ed24
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-03-16 11:14:13 +0000

    Refactored to seperate full? and empty? methods from dock and release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Sorry, dock filled." if @bikes.count >= 20
+    fail "Sorry, dock filled." if full?
     @bikes << bike
   end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
+  
 end
